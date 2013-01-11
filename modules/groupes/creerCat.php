5e8ca7750ed07596dddca71d9acd44c5fb75ef10
Iungmann <v.iungmann@gmail.com>
2013-01-11 18:05:58 +0100

    Resolution du bug sur afficher event

--- modules/groupes/creerCat.php
+++ modules/groupes/creerCat.php
@@ -1,29 +1,38 @@
 <?php
 $error = array();
-$nom = $_GET["nom"];
-$values = array("Libelle"=>"", "nom" => $nom);
 
-
-
-if(isset($_POST["filled"]) && $_POST["filled"] == "true") {
-
-	$bdd = new BDD();
-
-	if(isset($_POST["libelle"]) && !empty($_POST["libelle"]))
-		$values["libelle"] = $_POST["libelle"];
-	else
-		$error[] = "Vous devez donner un nom à votre catégorie...";
-
-	if(empty($error)) {
-		//Insertion de la nouvelle catégorie dans la table categorie
-		$result = $bdd->insert("categories", array("catLabel" => $_POST["libelle"]));
-		if(!$result)
-			$error[] = "Erreur insertion : ".$bdd->getLastError();
+if(isLogged()) {
+	
+	
+	$nom = $_GET["nom"];
+	$values = array("Libelle"=>"", "nom" => $nom);
+	
+	
+	
+	if(isset($_POST["filled"]) && $_POST["filled"] == "true") {
+	
+		$bdd = new BDD();
+	
+		if(isset($_POST["libelle"]) && !empty($_POST["libelle"]))
+			$values["libelle"] = $_POST["libelle"];
+		else
+			$error[] = "Vous devez donner un nom à votre catégorie...";
+	
+		if(empty($error)) {
+			//Insertion de la nouvelle catégorie dans la table categorie
+			$result = $bdd->insert("categories", array("catLabel" => $_POST["libelle"]));
+			if(!$result)
+				$error[] = "Erreur insertion : ".$bdd->getLastError();
+		}
+		
+		$bdd->close();
 	}
 	
-	$bdd->close();
+	
+	echo $twig->render("groupe_categorie.html", array("errors" => $error, "values" => $values));
 }
-
-
-echo $twig->render("groupe_categorie.html", array("errors" => $error, "values" => $values));
+else
+{
+	echo $twig->render("index_show.html", array());
+}
 
