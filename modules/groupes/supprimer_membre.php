069acc6a342f097ea24e8cdee221596f9a2d5d8e
Aurélien Bertron <aurelienbertron@gmail.com>
2013-01-12 23:24:43 +0100

    Corrige le problème des modals dans l'affichage des membres de groupes

--- modules/groupes/supprimer_membre.php
+++ modules/groupes/supprimer_membre.php
@@ -7,7 +7,8 @@ $error = "";
 $id=$_GET['id'];
 $idG = $_GET['idG'];
 
-$bdd->delete("own", array("member"=>$id, "grp"=>$idG ));
+$bdd->delete("Own", array("member"=>$id, "grp"=>$idG ));
 
+$bdd->close();
 
 echo $twig->render("groupes_supprimer_membre.html", array("id" => $idG));
