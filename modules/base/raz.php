bc638a9896b3071d9f4bdcac0d67e20fbc8adc17
Aurélien Bertron <aurelienbertron@gmail.com>
2013-01-12 18:38:03 +0100

    Intègre le module d'affichage de la base

--- modules/base/raz.php
+++ modules/base/raz.php
@@ -5,12 +5,8 @@ $errors[] = array();
 $bdd = new BDD();
 $bdd->select("COMMIT;");
 
-$fichier = file_get_contents(root()."/docs/RazBase.sql"); // on charge le fichier SQL
-// foreach($sql as $l){ // on le lit
-// 	if (substr(trim($l),0,2)!="--"){ // suppression des commentaires
-// 		$requetes .= $l;
-// 	}
-// }
+$fichier = file_get_contents(root()."/Docs/base.sql"); // on charge le fichier SQL
+
 try {
 	$bdd->getBDD()->exec($fichier);
 }
@@ -19,16 +15,6 @@ catch (PDOException $e) {
 	$bdd->select("ROLLBACK;");
 }
  
-// $reqs = split(";\n",$requetes);// on sépare les requêtes
-// print_r($reqs);
-// foreach($reqs as $req){ // et on les éxécute
-// 	if (!$bdd->select($req) && trim($req)!=""){
-// 		$errors[] = "Erreur de lecture du fichier";
-// 		$bdd->select("ROLLBACK;");
-// 		break;
-// 	}
-// 	var_dump($req);
-// }
 $bdd->close();
 
 echo $twig->render("base_raz.html", array());
--- templates/js/bootstrap-overlay.js
+++ templates/js/bootstrap-overlay.js
@@ -32,3 +32,60 @@ $(document).ready(function(){
 	  });
   });
 });
+
+
+
+/* ** cartouche ********************************************************************* */
+/* Script complet de gestion d'une requête de type XMLHttpRequest                     */
+/* Par Sébastien de la Marck (aka Thunderseb)                                         */
+/* ********************************************************************************** */
+
+function getXMLHttpRequest() {
+	var xhr = null;
+	
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	
+	return xhr;
+}
+
+
+
+
+/* **************************************************************************************** */
+/* Accordéon pour l'affichage du contenu des tables											*/
+/* **************************************************************************************** */
+
+function baseRequest(callback, tableName, targetFile) {
+	var xhr = getXMLHttpRequest();
+	
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+			callback(xhr.responseText, tableName);
+			$(".loader_image").css("display","none");
+		}
+		else {
+			$(".loader_image").css("display","inline");
+		}
+	};
+	
+	xhr.open("POST", targetFile, true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("ajax=true&table="+tableName);
+}
+
+function setAccordionBody(content, tableName) {
+	$("#"+tableName+" div").html(content);
+}
