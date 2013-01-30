6e29b156680ff4515dcad673adee8f2d506b14bb
Iungmann <v.iungmann@gmail.com>
2013-01-30 12:27:49 +0100

    Ajout de supprimerCat

---
+++ modules/groupes/supprimerCat.php
@@ -0,0 +1,29 @@
+<?php
+$error = array();
+$bdd = new BDD();
+$categories = null;
+$grp=$_GET["idGroupe"];
+
+if(isLogged() && isset($_GET["idGroupe"])) {
+	
+	//Suppression
+	if(isset($_POST["supr"]) && $_POST["supr"] == "true") {		
+		if(empty($error)) {
+
+			//Suppression catégorie dans la table Categories
+			$result = $bdd->delete("Categories", array( "catid" => $_POST["idcat"]  ));
+			if(!$result)
+				$error[] = "Erreur insertion : ".$bdd->getLastError();
+		}
+		//header("Location:index.php?module=groupes&action=afficher_groupes");
+		
+	}
+
+	//Affichage des catégories
+	$categories = $bdd->select("select c.catid, c.catlabel, c.grp from Categories c where c.grp='".$grp."';");
+	if(!$categories)
+		$errors[] = "Il n'y a rien à afficher";
+	
+	$bdd->close();
+	echo $twig->render("groupes_modifier_supprimerCat.html", array("id" => $grp, "categories" => $categories));
+}
