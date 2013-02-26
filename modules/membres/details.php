ecf735d2f82e66ccb267693cc2424d3a47b3f426
Sébastien <Sbastien@Mon-PC>
2013-02-26 21:41:19 +0100

    Ajoute les dernieres modifications qu'apparement j'avais oublie de commit... SORRY ABOUT THAT

--- modules/membres/details.php
+++ modules/membres/details.php
@@ -4,7 +4,7 @@ $errors = array();
 
 if(isset($_GET["membid"])) {
   $bdd = new BDD();
-  $membre = $bdd->select("select membid from Members where membid = ".$_GET["membid"].";");
+  $membre = $bdd->select("select membid, membfirstname, memblastname from Members where membid = ".$_GET["membid"].";");
   if(!$membre) {
     $errors[] = "Id inconnu";
   }
@@ -16,10 +16,11 @@ if(isset($_GET["membid"])) {
     
     //Requête permettant de récupérer les évènents du membre
     $bdd2 = new BDD();
-    $membevents = $bdd2->select("select p.event, p.member, e.eventid, e.eventname, e.grp, e.date, e.time, g.grpname 
-    							from participate as p, events as e, groups as g
+    $membevents = $bdd2->select("select p.event, p.member, e.eventid, e.eventname, e.grp, e.date, e.time, g.grpname, m.membid, m.membfirstname, m.memblastname
+    							from participate as p, events as e, groups as g, members as m
     							where p.event = e.eventid
-    							and e.grp = g.grpid;");
+    							and e.grp = g.grpid
+    							and p.member = m.membid;");
     
     echo $twig->render("membres_details.html", array("membid" => $membid, "membevents" => $membevents));
   }
